54b4cf6a07b833e6d4f522d14607117f2fa73411
lock-stock-fundamentals <98682991+lock-stock-fundamentals@users.noreply.github.com>
2022-01-30 17:40:54 +1000

    Update heroku_scheduler.py

--- heroku_scheduler.py
+++ heroku_scheduler.py
@@ -15,7 +15,7 @@ def job_function_2():
 def job_function_3():
     InsidersDeals().PerformAll()
 
-shed.add_job(job_function_1, 'cron', day_of_week='sun', hour=2, minute=5)  # sun (Sunday night : 2 ; 5)
+shed.add_job(job_function_1, 'cron', day_of_week='sun', hour=10, minute=45)  # sun (Sunday night : 2 ; 5)
 shed.add_job(job_function_2, 'cron', day_of_week='sun', hour=10, minute=10)  # sun (Sunday - Monday morning)
 # shed.add_job(job_function_3, 'cron', day_of_week='wed', hour=16, minute=24)  # set to !!! 8 and 5, sun (Sunday)
 shed.start()
