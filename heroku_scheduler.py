2a8adaf05261754874dd12c445d06d84d03f57bb
lock-stock-fundamentals <98682991+lock-stock-fundamentals@users.noreply.github.com>
2022-02-03 21:39:12 +1000

    Update heroku_scheduler.py

--- heroku_scheduler.py
+++ heroku_scheduler.py
@@ -2,8 +2,7 @@ import datetime
 from apscheduler.schedulers.blocking import BlockingScheduler
 from ranking_part_to_G import RankingClass as scf
 from ranking_part_to_G_2 import RankingClass as scf_2
-from w2w_total_change import RankingClass as wtc
-from parse_insiders_1 import InsidersDeals
+# from parse_insiders_1 import InsidersDeals
 
 shed = BlockingScheduler(timezone="Europe/Moscow")
 
@@ -11,16 +10,10 @@ def job_function_1():
     scf().spreadsheet_forming()
     
 def job_function_2():
-    scf_2().preparing_rank_sheets()
+    scf_2().PerformAll()
 
-def job_function_3():
-    wtc().total_change_calc()
     
-def job_function_4():
-    InsidersDeals().PerformAll()
-
 shed.add_job(job_function_1, 'cron', day_of_week='sun', hour=2, minute=5)  # sun (Sunday night : 2 ; 5)
 shed.add_job(job_function_2, 'cron', day_of_week='sun', hour=10, minute=5)  # sun (Sunday morning (say, 10 AM)
-shed.add_job(job_function_3, 'cron', day_of_week='mon', hour=16, minute=37)  # sun (Monday morning)
-# shed.add_job(job_function_4, 'cron', day_of_week='wed', hour=16, minute=24)  # set to !!! 8 and 5, sun (Sunday)
+# shed.add_job(job_function_3, 'cron', day_of_week='mon', hour=16, minute=37)  # sun (Monday morning)
 shed.start()
