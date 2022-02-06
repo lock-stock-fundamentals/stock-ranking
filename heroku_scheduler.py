e9de8a2b4562ab9c5396859316fd4e2ade1ff108
lock-stock-fundamentals <98682991+lock-stock-fundamentals@users.noreply.github.com>
2022-02-07 00:14:42 +1000

    Update heroku_scheduler.py

--- heroku_scheduler.py
+++ heroku_scheduler.py
@@ -7,13 +7,14 @@ from ranking_part_to_G_2 import RankingClass as scf_2
 shed = BlockingScheduler(timezone="Europe/Moscow")
 
 def job_function_1():
-    scf().spreadsheet_forming()
+    scf().backup_to_retro()
+    #scf().spreadsheet_forming()
     
 def job_function_2():
     scf_2().PerformAll()
 
     
-shed.add_job(job_function_1, 'cron', day_of_week='sun', hour=2, minute=5)  # sun (Sunday night : 2 ; 5)
+shed.add_job(job_function_1, 'cron', day_of_week='sun', hour=17, minute=18)  # sun (Sunday night : 2 ; 5)
 shed.add_job(job_function_2, 'cron', day_of_week='sun', hour=10, minute=5)  # sun (Sunday morning (say, 10 AM)
 # shed.add_job(job_function_3, 'cron', day_of_week='mon', hour=16, minute=37)  # sun (Monday morning)
 shed.start()
