9c7b59e7f8096d47c2e63bc63171afebbf50b928
lock-stock-fundamentals <98682991+lock-stock-fundamentals@users.noreply.github.com>
2022-02-15 17:11:22 +1000

    Update heroku_scheduler.py

--- heroku_scheduler.py
+++ heroku_scheduler.py
@@ -1,17 +1,20 @@
 import datetime, time
 from apscheduler.schedulers.blocking import BlockingScheduler
 from ranking_part_to_G import RankingClass as scf
+from rank_G_double_check import RankingClass as scf_double_check
 from ranking_part_to_G_2 import RankingClass as scf_2
 # from parse_insiders_1 import InsidersDeals
-
-shed = BlockingScheduler(timezone="Europe/Moscow")
 
 def job_function_1():
     scf().spreadsheet_forming()
     time.sleep(10)
+    scf_double_check.spreadsheet_forming_after_check()
+    time.sleep(10)
     scf_2().PerformAll() # perform the 2-nd part right after the first one
+
+shed = BlockingScheduler(timezone="Europe/Moscow")
       
-shed.add_job(job_function_1, 'cron', day_of_week='tue', hour=9, minute=55) 
+shed.add_job(job_function_1, 'cron', day_of_week='sun', hour=6, minute=5) 
 # shed.add_job(job_function_2, 'cron', day_of_week='sun', hour=10, minute=5)  # sun (Sunday morning (say, 10 AM)
 # shed.add_job(job_function_3, 'cron', day_of_week='mon', hour=16, minute=37)  # sun (Monday morning)
 shed.start()
